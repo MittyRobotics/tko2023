46d75bffabc70174b38d031a482eb3163855e7fb
programming1351 <programming@amhsrobotics.com>
2023-02-04 14:38:09 -0800

    add logger?

--- src/main/java/com/github/mittyrobotics/LoggerInterface.java
+++ src/main/java/com/github/mittyrobotics/LoggerInterface.java
@@ -1,36 +1,36 @@
 package com.github.mittyrobotics;
 
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.networktables.NetworkTableValue;
+import edu.wpi.first.networktables.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class LoggerInterface {
     private final NetworkTableInstance nt;
     private final NetworkTable table;
+    private final HashMap<String, StringPublisher> pubs = new HashMap<>();
 
     public LoggerInterface() {
         nt = NetworkTableInstance.getDefault();
-        nt.setServerTeam(1351);
-        nt.startDSClient();
+        nt.startServer();
+//        nt.setServerTeam(1351);
+//        nt.startDSClient();
 
         table = nt.getTable("Log");
+
+        System.out.println(nt.isConnected());
     }
 
     public void put(String key, Object value) {
-        if (table.getValue(key).getValue() == null) {
-            table.putValue(key, NetworkTableValue.makeStringArray(new String[]{System.currentTimeMillis() + "|" + value}));
+        if (pubs.containsKey(key)) {
+            pubs.get(key).set(String.valueOf(value), System.currentTimeMillis());
         } else {
-            System.out.println(Arrays.toString(table.getValue(key).getStringArray()));
-
-            ArrayList<String> s = new ArrayList<>(List.of(table.getValue(key).getStringArray()));
-            s.add(System.currentTimeMillis() + "|" + value);
-            System.out.println(s);
-
-            table.putValue(key, NetworkTableValue.makeStringArray(Arrays.copyOf(s.toArray(), s.toArray().length, String[].class)));
+            StringTopic dt = table.getStringTopic(key);
+            StringPublisher pub = dt.publish(PubSubOption.keepDuplicates(true));
+            pub.set(String.valueOf(value), System.currentTimeMillis());
+            pubs.put(key, pub);
         }
     }
 }
--- src/main/java/com/github/mittyrobotics/RobotTestRev.java
+++ src/main/java/com/github/mittyrobotics/RobotTestRev.java
@@ -14,6 +14,8 @@ public class RobotTestRev extends TimedRobot {
     private RelativeEncoder m_encoder;
     public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, maxRPM, maxVel, minVel, maxAcc, allowedErr;
 
+    public LoggerInterface log;
+
     protected RobotTestRev() {
         super();
     }
@@ -24,56 +26,58 @@ public class RobotTestRev extends TimedRobot {
         // initialize motor
         m_motor = new CANSparkMax(deviceID, CANSparkMaxLowLevel.MotorType.kBrushless);
 
+        log = new LoggerInterface();
+
         /**
          * The RestoreFactoryDefaults method can be used to reset the configuration parameters
          * in the SPARK MAX to their factory default state. If no argument is passed, these
          * parameters will not persist between power cycles
          */
-        m_motor.restoreFactoryDefaults();
-        m_motor.getEncoder().setPosition(0);
-
-        // initialze PID controller and encoder objects
-        m_pidController = m_motor.getPIDController();
-        m_encoder = m_motor.getEncoder();
-
-        // PID coefficients
-        kP = 1;
-        kI = 0.0;
-        kD = 0.0;
-        kFF = 1/6080. ;
-        kMaxOutput = 0.3;
-        kMinOutput = -0.3;
-        maxRPM = 5700;
-
-        // Smart Motion Coefficients
-        maxVel = 3000; // rpm
-        maxAcc = 1500;
-        minVel = 100;
-
-        // set PID coefficients
-        m_pidController.setP(kP);
-        m_pidController.setI(kI);
-        m_pidController.setD(kD);
-        m_pidController.setFF(kFF);
-//        m_pidController.setOutputRange(kMinOutput, kMaxOutput);
-
-        /**
-         * Smart Motion coefficients are set on a SparkMaxPIDController object
-         *
-         * - setSmartMotionMaxVelocity() will limit the velocity in RPM of
-         * the pid controller in Smart Motion mode
-         * - setSmartMotionMinOutputVelocity() will put a lower bound in
-         * RPM of the pid controller in Smart Motion mode
-         * - setSmartMotionMaxAccel() will limit the acceleration in RPM^2
-         * of the pid controller in Smart Motion mode
-         * - setSmartMotionAllowedClosedLoopError() will set the max allowed
-         * error for the pid controller in Smart Motion mode
-         */
-        int smartMotionSlot = 0;
-        m_pidController.setSmartMotionMaxVelocity(maxVel, smartMotionSlot);
-        m_pidController.setSmartMotionMinOutputVelocity(minVel, smartMotionSlot);
-        m_pidController.setSmartMotionMaxAccel(maxAcc, smartMotionSlot);
-        m_pidController.setSmartMotionAllowedClosedLoopError(allowedErr, smartMotionSlot);
+//        m_motor.restoreFactoryDefaults();
+//        m_motor.getEncoder().setPosition(0);
+//
+//        // initialze PID controller and encoder objects
+//        m_pidController = m_motor.getPIDController();
+//        m_encoder = m_motor.getEncoder();
+//
+//        // PID coefficients
+//        kP = 1;
+//        kI = 0.0;
+//        kD = 0.0;
+//        kFF = 1/6080. ;
+//        kMaxOutput = 0.3;
+//        kMinOutput = -0.3;
+//        maxRPM = 5700;
+//
+//        // Smart Motion Coefficients
+//        maxVel = 3000; // rpm
+//        maxAcc = 1500;
+//        minVel = 100;
+//
+//        // set PID coefficients
+//        m_pidController.setP(kP);
+//        m_pidController.setI(kI);
+//        m_pidController.setD(kD);
+//        m_pidController.setFF(kFF);
+////        m_pidController.setOutputRange(kMinOutput, kMaxOutput);
+//
+//        /**
+//         * Smart Motion coefficients are set on a SparkMaxPIDController object
+//         *
+//         * - setSmartMotionMaxVelocity() will limit the velocity in RPM of
+//         * the pid controller in Smart Motion mode
+//         * - setSmartMotionMinOutputVelocity() will put a lower bound in
+//         * RPM of the pid controller in Smart Motion mode
+//         * - setSmartMotionMaxAccel() will limit the acceleration in RPM^2
+//         * of the pid controller in Smart Motion mode
+//         * - setSmartMotionAllowedClosedLoopError() will set the max allowed
+//         * error for the pid controller in Smart Motion mode
+//         */
+//        int smartMotionSlot = 0;
+//        m_pidController.setSmartMotionMaxVelocity(maxVel, smartMotionSlot);
+//        m_pidController.setSmartMotionMinOutputVelocity(minVel, smartMotionSlot);
+//        m_pidController.setSmartMotionMaxAccel(maxAcc, smartMotionSlot);
+//        m_pidController.setSmartMotionAllowedClosedLoopError(allowedErr, smartMotionSlot);
     }
 
     @Override
@@ -89,6 +93,7 @@ public class RobotTestRev extends TimedRobot {
     @Override
     public void robotPeriodic() {
         super.robotPeriodic();
+        log.put("Test", m_motor.getEncoder().getPosition());
     }
 
     @Override
@@ -105,8 +110,8 @@ public class RobotTestRev extends TimedRobot {
     public void teleopPeriodic() {
         SmartDashboard.putNumber("VEL", m_motor.getEncoder().getVelocity());
         SmartDashboard.putNumber("OUTPUT", m_motor.getAppliedOutput());
-        m_pidController.setReference(1500, CANSparkMax.ControlType.kSmartMotion);
-        System.out.println("POS: " + m_encoder.getPosition());
+//        m_pidController.setReference(1500, CANSparkMax.ControlType.kSmartMotion);
+        System.out.println("POS: " + m_motor.getEncoder().getPosition());
     }
 
 }
