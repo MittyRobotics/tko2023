4b8f1cdbad50661bbc53a7975304af88b0d995fd
DivyanshAgarwal24 <82910642+DivyanshAgarwal24@users.noreply.github.com>
2023-07-08 13:06:18 -0700

    add operator OI

---
+++ src/main/java/com/github/mittyrobotics/util/OIConstants.java
@@ -0,0 +1,6 @@
+package com.github.mittyrobotics.util;
+
+public class OIConstants {
+    public static final int DRIVER_CONTROLLER_ID = 0;
+    public static final int OPERATOR_CONTROLLER_ID = 1;
+}
