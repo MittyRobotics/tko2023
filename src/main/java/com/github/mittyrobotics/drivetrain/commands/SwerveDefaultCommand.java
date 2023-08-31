7c81a74eddef2cbb68f7a24d5f09dec3cbdf12c2
DivyanshAgarwal24 <82910642+DivyanshAgarwal24@users.noreply.github.com>
2023-08-31 16:37:12 -0700

    revert

--- src/main/java/com/github/mittyrobotics/drivetrain/commands/SwerveDefaultCommand.java
+++ src/main/java/com/github/mittyrobotics/drivetrain/commands/SwerveDefaultCommand.java
@@ -26,7 +26,50 @@ public class SwerveDefaultCommand extends CommandBase {
 
     @Override
     public void execute() {
+        throttleX = -OI.getInstance().getDriverController().getLeftY();
+        throttleY = -OI.getInstance().getDriverController().getLeftX();
+        throttleAngular = -OI.getInstance().getDriverController().getRightX();
 
+        SwerveSubsystem.getInstance().calculateInputs(
+                new Vector(
+                        SwerveConstants.MAX_LINEAR_SPEED_INCHES_PER_SECOND * Math.abs(throttleX) * throttleX,
+                        SwerveConstants.MAX_LINEAR_SPEED_INCHES_PER_SECOND * Math.abs(throttleY) * throttleY
+                ),
+                SwerveConstants.MAX_ANGULAR_SPEED * throttleAngular
+        );
+
+        SwerveSubsystem.getInstance().applyCalculatedInputs();
+
+        SmartDashboard.putNumber("Gyro", Gyro.getInstance().getHeadingRadians());
+
+        double[] desiredAngles = SwerveSubsystem.getInstance().getDesiredAngles(),
+                desiredMagnitudes = SwerveSubsystem.getInstance().getDesiredMagnitudes();
+        SmartDashboard.putString("Wheel 1", new Vector(new Angle(desiredAngles[0], true),
+                desiredMagnitudes[0]).toString());
+        SmartDashboard.putString("Wheel 2", new Vector(new Angle(desiredAngles[1], true),
+                desiredMagnitudes[1]).toString());
+        SmartDashboard.putString("Wheel 3", new Vector(new Angle(desiredAngles[2], true),
+                desiredMagnitudes[2]).toString());
+        SmartDashboard.putString("Wheel 4", new Vector(new Angle(desiredAngles[3], true),
+                desiredMagnitudes[3]).toString());
+
+        SmartDashboard.putNumber("Wheel 1 angle", desiredAngles[0]);
+        SmartDashboard.putNumber("Wheel 2 angle", desiredAngles[1]);
+        SmartDashboard.putNumber("Wheel 3 angle", desiredAngles[2]);
+        SmartDashboard.putNumber("Wheel 4 angle", desiredAngles[3]);
+
+        SmartDashboard.putBoolean("Wheel 1 flip", SwerveSubsystem.getInstance().flipped[0]);
+        SmartDashboard.putBoolean("Wheel 2 flip", SwerveSubsystem.getInstance().flipped[1]);
+        SmartDashboard.putBoolean("Wheel 3 flip", SwerveSubsystem.getInstance().flipped[2]);
+        SmartDashboard.putBoolean("Wheel 4 flip", SwerveSubsystem.getInstance().flipped[3]);
+
+//        SmartDashboard.putNumber("wheel 1 ticks", SwerveSubsystem.getInstance().getRawPosition(0));
+        SmartDashboard.putNumber("wheel 1 position", SwerveSubsystem.getInstance().getEncoderModuleAngle(0));
+        SmartDashboard.putNumber("wheel 2 position", SwerveSubsystem.getInstance().getEncoderModuleAngle(1));
+        SmartDashboard.putNumber("wheel 3 position", SwerveSubsystem.getInstance().getEncoderModuleAngle(2));
+        SmartDashboard.putNumber("wheel 4 position", SwerveSubsystem.getInstance().getEncoderModuleAngle(3));
+
+        SmartDashboard.putNumber("wheel 1 velocity", SwerveSubsystem.getInstance().getRawWheelVelocity(0));
     }
 
     @Override
