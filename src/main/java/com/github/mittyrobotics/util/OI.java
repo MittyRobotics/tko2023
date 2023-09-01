ed4434d5e1133f0c6a24a6e8c8f1956ffbd9e962
DivyanshAgarwal24 <82910642+DivyanshAgarwal24@users.noreply.github.com>
2023-08-31 17:38:01 -0700

    changes

--- src/main/java/com/github/mittyrobotics/util/OI.java
+++ src/main/java/com/github/mittyrobotics/util/OI.java
@@ -6,8 +6,6 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
-import javax.swing.plaf.nimbus.State;
-
 import static com.github.mittyrobotics.util.OIConstants.*;
 
 public class OI {
