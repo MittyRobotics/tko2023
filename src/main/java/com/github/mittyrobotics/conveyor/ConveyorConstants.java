fd0300ec44aea76baac2be4ee0decd0f9dd399aa
DivyanshAgarwal24 <82910642+DivyanshAgarwal24@users.noreply.github.com>
2023-10-07 20:20:40 -0700

    clear for new install

--- src/main/java/com/github/mittyrobotics/conveyor/ConveyorConstants.java
+++
@@ -1,9 +0,0 @@
-package com.github.mittyrobotics.conveyor;
-
-public class ConveyorConstants {
-    public static final int MOTOR_ID = 20;
-
-    public static final double INCHES_PER_REV = 0;
-
-    public static final int LIMIT_SWITCH_ID = 30;
-}
