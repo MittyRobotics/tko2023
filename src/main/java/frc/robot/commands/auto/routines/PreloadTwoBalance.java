afdaff38513021b8f0bb9ef93d6ce0c248d76f63
DivyanshAgarwal24 <82910642+DivyanshAgarwal24@users.noreply.github.com>
2023-10-19 17:35:03 -0700

    commit

--- src/main/java/frc/robot/commands/auto/routines/PreloadTwoBalance.java
+++ src/main/java/frc/robot/commands/auto/routines/PreloadTwoBalance.java
@@ -18,36 +18,13 @@ public class PreloadTwoBalance extends AutoRoutine {
                              Boolean low) {
         super();
 
-        SwervePath firstPiecePath = pathManager.paths.get(
-                low == null ? null : low ? LOW_TO_FIRST_PIECE : HIGH_TO_FIRST_PIECE);
-        SwervePath firstScorePath = pathManager.paths.get(
-                low == null ? null : low ? LOW_FIRST_PIECE_TO_SCORE : HIGH_FIRST_PIECE_TO_SCORE);
-        SwervePath secondPiecePath = pathManager.paths.get(
-                low == null ? null : low ? LOW_TO_SECOND_PIECE : HIGH_TO_SECOND_PIECE);
-        SwervePath secondScorePath = pathManager.paths.get(
-                low == null ? null : low ? LOW_SECOND_PIECE_TO_SCORE : HIGH_SECOND_PIECE_TO_SCORE);
         SwervePath balancePath = pathManager.paths.get(
                 low == null ? null : low ? LOW_BALANCE_FAR_SIDE : HIGH_BALANCE_FAR_SIDE);
         addCommands(
-                new AutoScoreHigh(conveyor, shooter),
-                new PathFollowingCommand(swerve, gyro, poseEstimator, firstPiecePath),
-                new LowerIntake(intake),
-                new ParallelCommandGroup(
-                        new PathFollowingCommand(swerve, gyro, poseEstimator, low == null ? null : pathManager, 10),
-                        new BringCubeToHolding(conveyor)
-                ),
-                new RaiseIntake(intake),
-                new PathFollowingCommand(swerve, gyro, poseEstimator, firstScorePath),
-                new AutoScoreMid(conveyor, shooter),
-                new PathFollowingCommand(swerve, gyro, poseEstimator, secondPiecePath),
-                new LowerIntake(intake),
-                new ParallelCommandGroup(
-                        new PathFollowingCommand(swerve, gyro, poseEstimator, low == null ? null : pathManager, 10),
-                        new BringCubeToHolding(conveyor)
-                ),
-                new RaiseIntake(intake),
-                new PathFollowingCommand(swerve, gyro, poseEstimator, secondScorePath),
-                new AutoScoreHybrid(conveyor, intake),
+                new PreloadTwo(pathManager,
+                        swerve, gyro, poseEstimator,
+                        conveyor, shooter, intake,
+                        low),
                 new PathFollowingCommand(swerve, gyro, poseEstimator, balancePath),
                 new AutoBalance(swerve, gyro, poseEstimator, low == null)
         );
